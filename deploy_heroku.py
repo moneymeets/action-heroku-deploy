240bb72050738fd44d62cfea9b4ae0fb81b77c93
Marcel Arns <marcel.arns@moneymeets.com>
2020-11-16 16:18:02 +0100

    fixup! fixup! chore(poetry): bump dependencies

--- deploy_heroku.py
+++ deploy_heroku.py
@@ -1,21 +1,12 @@
 import argparse
+import json
 import os
 import subprocess
-import dataclasses
+import urllib.request
 from typing import List
 
-import json
-import urllib.request
 
-
-@dataclasses.dataclass
-class HerokuRelease:
-    id: str
-    version: int
-    status: str
-
-
-def get_latest_heroku_release(app: str, api_key: str) -> HerokuRelease:
+def get_latest_heroku_release(app: str, api_key: str):
     request = urllib.request.Request(f"https://api.heroku.com/apps/{app}/releases")
     for key, value in (
             ("Accept", "application/vnd.heroku+json; version=3"),
@@ -26,16 +17,15 @@ def get_latest_heroku_release(app: str, api_key: str) -> HerokuRelease:
         request.add_header(key, value)
 
     with urllib.request.urlopen(request) as response:
-        assert response.getcode() == 206, response.getcode()
-        latest_release, *_ = json.loads(response.read())
-        return HerokuRelease(**{prop.name: latest_release[prop.name] for prop in dataclasses.fields(HerokuRelease)})
+        assert response.getcode() == 200, response.getcode()
+        return json.loads(response.read())[0]
 
 
 def deploy_heroku_command(commit_hash: str, api_key: str, app: str) -> List[str]:
     return ["git", "push", f"https://heroku:{api_key}@git.heroku.com/{app}.git", f"{commit_hash}:refs/heads/master"]
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="perform deployment to Heroku")
     parser.add_argument("-a", "--app", default=os.environ.get("APP"), type=str)
     parser.add_argument("-K", "--api-key", default=os.environ.get("API_KEY"), type=str)
@@ -48,7 +38,7 @@ if __name__ == '__main__':
 
     print(f"Heroku app name: {heroku_app}")
 
-    old_release_version = get_latest_heroku_release(heroku_app, heroku_api_key).version
+    old_release_version = get_latest_heroku_release(heroku_app, heroku_api_key)["version"]
     print(f"Old release version is {old_release_version}")
 
     deploy_command = deploy_heroku_command(git_commit_hash, heroku_api_key, heroku_app)
@@ -57,8 +47,8 @@ if __name__ == '__main__':
 
     latest_heroku_release = get_latest_heroku_release(heroku_app, heroku_api_key)
 
-    if latest_heroku_release.status != "succeeded":
+    if latest_heroku_release["status"] != "succeeded":
         raise RuntimeError("Heroku release command failed! See Heroku release logs for detailed information.")
 
-    print(f"New release version is {latest_heroku_release.version}")
-    print(f"::set-output name=release_version::{latest_heroku_release.version}")
+    print(f"New release version is {latest_heroku_release['version']}")
+    print(f"::set-output name=release_version::{latest_heroku_release['version']}")
--- test_deploy_heroku.py
+++ test_deploy_heroku.py
@@ -1,8 +1,8 @@
 import json
 import unittest
-from unittest.mock import patch, MagicMock, call
+from unittest.mock import MagicMock, call, patch
 
-from deploy_heroku import get_latest_heroku_release
+from deploy_heroku import deploy_heroku_command, get_latest_heroku_release
 
 
 class DeployHerokuTestCase(unittest.TestCase):
@@ -14,12 +14,12 @@ class DeployHerokuTestCase(unittest.TestCase):
 
         cm = MagicMock()
         cm.getcode.return_value, cm.read.return_value, cm.__enter__.return_value = (
-            206,
+            200,
             json.dumps(
                 [
                     {"id": release_id, "version": release_version, "status": release_status},
                     {"id": release_id, "version": release_version - 1, "status": release_status},
-                ]
+                ],
             ),
             cm,
         )
@@ -30,9 +30,9 @@ class DeployHerokuTestCase(unittest.TestCase):
         mock_request.assert_called_once_with("https://api.heroku.com/apps/foo/releases")
         mock_request().add_header.assert_has_calls([call("Authorization", "Bearer bar")], any_order=True)
 
-        self.assertEqual(latest_release.id, release_id)
-        self.assertEqual(latest_release.version, release_version)
-        self.assertEqual(latest_release.status, release_status)
+        self.assertEqual(latest_release["id"], release_id)
+        self.assertEqual(latest_release["version"], release_version)
+        self.assertEqual(latest_release["status"], release_status)
 
     @patch("urllib.request.urlopen")
     def test_get_latest_heroku_release_fail(self, mock_urlopen):
@@ -41,6 +41,11 @@ class DeployHerokuTestCase(unittest.TestCase):
         mock_urlopen.return_value = cm
 
         self.assertRaises(AssertionError, get_latest_heroku_release, "foo", "bar")
+
+    def test_deploy_heroku_command(self):
+        expected_result = ["git", "push", "https://heroku:bar@git.heroku.com/foobar.git", "sha:refs/heads/master"]
+        result = deploy_heroku_command("sha", "bar", "foobar")
+        self.assertEqual(result, expected_result)
 
 
 if __name__ == "__main__":
